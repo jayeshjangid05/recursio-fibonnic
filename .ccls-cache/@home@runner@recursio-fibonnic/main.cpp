6f39a26d708be848ec8801cc1478361e80ab0fcd
jayeshjangid05 <>
2023-02-05 15:39:45 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@recursio-fibonnic/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int summation(int n) {
+  if (n == 1) {
+    return 1;
+  }
+  int first_term = summation(n - 1);
+  cout << first_term << endl;
+  return first_term + n;
+}
+int sum(int n) {
+  if (n == 1) {
+    return 1;
+  }
+  return n + sum(n - 1);
+}
+int recursion(int n) {
+  if (n == 1) {
+    return 1;
+  }
+  return n * recursion(n - 1);
+}
+int main() {
+  cout << "Factorial is  " << recursion(4) << endl;
+  cout << "Summation is  " << sum(5) << endl;
+  cout << "Print each step of summation " << endl;
+  cout << summation(6);
+  return 0;
+}
